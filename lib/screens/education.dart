fd28fb766a08dd0d4161d0c2b40d6454108bc4b7
ybendev <yusadulkadir@gmail.com>
2025-06-17 22:53:48 +0300

    renk ayarı2

--- lib/screens/education.dart
+++ lib/screens/education.dart
@@ -32,7 +32,7 @@ class EducationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF5F5F5),
+      backgroundColor: Color(0xFFF7F6E7),
       appBar: AppBar(
         backgroundColor: Color(0xFF8D6E63),
         title: const Text(
